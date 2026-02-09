25375ac73fa6cb0e91bf1c3633d670f94ca11006
Charlie <hanfeis@plexera.com>
2026-02-09 18:26:42 +0800

    多层膜计算 Matlab参考代码

--- MaterialsAndEquations/MaterialsAndEquations/Equations.cs
+++ MaterialsAndEquations/MaterialsAndEquations/Equations.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Numerics;
 using System.Text;
@@ -33,8 +34,34 @@ namespace MaterialsAndEquations
         }
 
 
-
-
+        //Matlab code for reference:
+        //lambda=660e-9;
+        //n0=1.721;
+        //n1=2.25432+3.63712i;
+        //n2=0.20026+3.62238i;
+        //n3=0.065+3.7793i;
+        //n4=n2;
+        //n5=1.333;
+        //d1=5e-9;d2=2e-9;d3=42e-9;d4=13e-9;theta=linspace(51.35,57.6,10000);
+        //        k0=2*pi* n0/lambda* cos(theta* pi/180);
+        //        k1=2*pi* sqrt((n1/lambda )^2-(n0/lambda* sin(theta* pi/180 ) ).^2 );
+        //k2=2*pi* sqrt((n2/lambda )^2-(n0/lambda* sin(theta* pi/180 ) ).^2 );
+        //k3=2*pi* sqrt((n3/lambda )^2-(n0/lambda* sin(theta* pi/180 ) ).^2 );
+        //k4=2*pi* sqrt((n4/lambda )^2-(n0/lambda* sin(theta* pi/180 ) ).^2 );
+        //k5=2*pi* sqrt((n5/lambda )^2-(n0/lambda* sin(theta* pi/180 ) ).^2 );
+        //p=2;
+        //s=0;
+        //polar=p;
+        //rou01=(n1^polar* k0-n0^polar* k1)./(n1^polar* k0+n0^polar* k1);
+        //        rou12=(n2^polar* k1-n1^polar* k2)./(n2^polar* k1+n1^polar* k2);
+        //        rou23=(n3^polar* k2-n2^polar* k3)./(n3^polar* k2+n2^polar* k3);
+        //        rou34=(n4^polar* k3-n3^polar* k4)./(n4^polar* k3+n3^polar* k4);
+        //        rou45=(n5^polar* k4-n4^polar* k5)./(n5^polar* k4+n4^polar* k5);
+        //        rou345=(rou34+rou45.* exp(2*i* k4*d4))./(1+rou34.* rou45.*exp(2*i* k4*d4));
+        //rou2345=(rou23+rou345.* exp(2*i* k3*d3))./(1+rou23.* rou345.*exp(2*i* k3*d3));
+        //rou12345=(rou12+rou2345.* exp(2*i* k2*d2))./(1+rou12.* rou2345.*exp(2*i* k2*d2));
+        //rou012345=(rou01+rou12345.* exp(2*i* k1*d1))./(1+rou01.* rou12345.*exp(2*i* k1*d1))
+        //R=abs(rou012345).^2;R=R/max(R);
 
     }
 }
